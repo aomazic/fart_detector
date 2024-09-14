c7ba9cbd7eb0fa3378ac1fc3a6846abb0e1ba20d
aomazic <toniomazic7@gmail.com>
2024-09-14 15:04:10 +0200

    Fixed sizing

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,58 +8,64 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: buildContainer(context),
+      body: buildBody(),
     );
   }
 
-  Container buildContainer(BuildContext context) {
-    final double radarHeight = MediaQuery.of(context).size.height * 0.5;
+  Widget buildBody() {
+    return Column(
+      children: [
+        Expanded(
+          flex: 3,
+          child: buildRadar(),
+        ),
+        Expanded(
+          flex: 2,
+          child: buildInfoPanel(),
+        ),
+      ],
+    );
+  }
+
+  Widget buildRadar() {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        border: Border.all(
+          color: const Color(0xFF2AB82D),
+          width: 1.0,
+        ),
+      ),
+      child: CustomPaint(
+        painter: RadarPainter(),
+      ),
+    );
+  }
+
+  Widget buildInfoPanel() {
     return Container(
       color: Colors.black,
-      child: Stack(
+      padding: const EdgeInsets.all(8.0),
+      child: const Stack(
         children: [
-          buildRadar(radarHeight),
-          buildInfoPanel(),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Range: 0m-5m',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Text(
+              '0m',
+              textAlign: TextAlign.right,
+            ),
+          ),
         ],
       ),
     );
-  }
-
-  Positioned buildInfoPanel() {
-    return Positioned(
-      bottom: 0,
-      left: 0,
-      right: 0,
-      child: Container(
-        color: Colors.black,
-        padding: const EdgeInsets.all(8.0),
-        child: const Text(
-          'Range: 0m-5m',
-          textAlign: TextAlign.center,
-        ),
-
-      ),
-    );
-  }
-
-  Positioned buildRadar(double radarHeight) {
-    return Positioned(
-          top: 0,
-          left: 0,
-          right: 0,
-          height: radarHeight, // Adjust the height as needed
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: const Color(0xFF2AB82D),
-                width: 1.0,
-              ),
-            ),
-            child: CustomPaint(
-              painter: RadarPainter(),
-            ),
-          ),
-        );
   }
 
   AppBar buildAppBar() {
@@ -68,10 +74,10 @@ class HomePage extends StatelessWidget {
       title: const Padding(
         padding: EdgeInsets.only(top: 10.0),
         child: Text(
-          'Fart Detector',
+          'Fart Detector ver. 0.58',
           style: TextStyle(
             color: Color(0xFF2AB82D),
-            fontSize: 22,
+            fontSize: 18,
           ),
         ),
       ),
